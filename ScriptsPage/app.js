da1ab7495db15effcbb386f8c9c899588fd2469e
fernancat <fernancarranza90@gmail.com>
2024-02-14 18:19:02 -0600

    desencriptar y responsive

--- ScriptsPage/app.js
+++ ScriptsPage/app.js
@@ -9,16 +9,20 @@ let diccionario = {
     "u": "ufat",
    }
 
-
+let diccionarioInverso = {};
+for (let clave in diccionario) {
+    diccionarioInverso[diccionario[clave]] = clave;
+}
 
 
 function encriptar(){
-   let cadenaGenerada = "";
    let textoArea = elementoArea.value; 
    let cadena = "";
    
+
    for (i = 0; i < textoArea.length; i++){
       if(diccionario[textoArea[i]] != undefined){
+         console.log(diccionario[textoArea[i]])
          cadena+= diccionario[textoArea[i]]
       }else{
          cadena+=textoArea[i]
@@ -30,8 +34,32 @@ function encriptar(){
 }
 
 
-function desencriptar(){
+function desencriptar(texto){
+   textoArea = elementoArea.value
 
+   let textoAreaDos = elementoAreaDos.value;
+   let cadena = "";
+
+   for (let clave in diccionarioInverso) {
+      textoAreaDos = textoAreaDos.replaceAll(clave, diccionarioInverso[clave]);
+   }
+
+   elementoAreaDos.value = textoAreaDos;
+  
+
+}
+
+function copiar(){
+   navigator.clipboard.writeText(elementoAreaDos.value)
+   alert("texto copiado!")
+}
+
+function verificarMayuscula(){
+   let textoArea = elementoArea.value;
+
+   if (/[A-Z]/.test(textoArea)){
+      return true;
+   }
 }
 
 
